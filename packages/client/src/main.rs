32a952a943f54b3e3f6a433249bf7cb9c678c374
Szymon Piechaczek <szymon@piechaczek.it>
2024-03-22 00:03:07 +0100

    added proto for ble characteristics read

--- packages/client/src/main.rs
+++ packages/client/src/main.rs
@@ -1,7 +1,17 @@
-use std::io::Error;
+use std::collections::HashSet;
 use std::process::exit;
-use tonic;
+
+use bluster::gatt::characteristic::{Characteristic, Properties};
+use bluster::gatt::characteristic;
+use bluster::gatt::event::Event;
+use bluster::gatt::service::Service;
+use bluster::Peripheral;
 use clap::Parser;
+use futures_channel::mpsc::channel;
+use tonic;
+use tonic::codegen::tokio_stream::StreamExt;
+use uuid::Uuid;
+
 use ble_over_ip_proto::ble_proxy_client::BleProxyClient;
 use ble_over_ip_proto::DiscoverRequest;
 
@@ -22,10 +32,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let connection_result = BleProxyClient::connect(args.url).await;
 
-    if (connection_result.is_err()) {
+    if connection_result.is_err() {
         eprintln!("Could not connect: {}", connection_result.err().unwrap());
         exit(1);
-        return Ok(())
     }
 
     let mut client = connection_result.unwrap();
@@ -34,17 +43,94 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let discovery_request = tonic::Request::new(DiscoverRequest {});
     let discovery_response = client.discover_device(discovery_request).await?;
+    let discovery = discovery_response.into_inner();
 
-    let services = discovery_response.into_inner().services;
-    println!("Services length: {}", services.len());
-    for s in services {
+
+    println!("Starting peripheral device...");
+
+    let peripheral_result = Peripheral::new().await;
+    if peripheral_result.is_err() {
+        eprintln!("Could not create peripheral device: {}", peripheral_result.err().unwrap());
+        exit(1);
+    }
+    let peripheral = peripheral_result.unwrap();
+
+    let (sender, receiver) = channel(1);
+    for s in &discovery.services {
         println!("├─ Service {}", s.uuid);
-        for c in s.characteristics {
+        let mut characteristics: HashSet<Characteristic> = HashSet::new();
+        for c in &s.characteristics {
             println!("├── Characteristic {}", c.uuid);
+            characteristics.insert(Characteristic::new(
+                c.uuid.parse().unwrap(),
+                Properties::new(
+                    if c.can_read {
+                        Some(
+                        characteristic::Read(
+                            characteristic::Secure::Insecure(sender.clone()))
+                    )
+                    } else { None },
+                    if c.can_write {
+                        Some(
+                            characteristic::Write::WithoutResponse(
+                                sender.clone()
+                            )
+                        )
+                    } else { None },
+                    if c.can_subscribe {
+                        Some(
+                            sender.clone()
+                        )
+                    } else { None },
+                    if c.can_subscribe {
+                        Some(sender.clone())
+                    } else { None },
+                ),
+                None,
+                HashSet::new(),
+            ));
         }
+        let p_service = Service::new(
+            s.uuid.parse().unwrap(),
+            true,
+            characteristics,
+        );
+        peripheral.add_service(&p_service).unwrap();
     }
 
-    println!("Discovery finished!");
+    let characteristic_handler = async {
+        let mut rx = receiver;
 
+        while let Some(event) = rx.next().await {
+            // process request from client device
+            match event {
+                Event::ReadRequest(read_request) => {
+
+                }
+                Event::WriteRequest(write_request) => {
+
+                }
+                Event::NotifySubscribe(notify_subscribe) => {
+
+                }
+                Event::NotifyUnsubscribe => {
+
+                }
+            }
+
+        }
+    };
+
+
+    let mut uuids: Vec<Uuid> = vec![];
+    for s in &discovery.services {
+        uuids.push(s.uuid.parse().unwrap())
+    }
+
+    println!("Starting advertising!");
+    peripheral.start_advertising(&discovery.name, uuids.as_slice()).await?;
+
+
+    println!("Finished!");
     Ok(())
 }
